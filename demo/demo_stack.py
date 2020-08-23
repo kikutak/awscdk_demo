1b4cbccda968c7f750343c0862696a48db6a7f8b
kikutak <reg.takonorigame@gmail.com>
2020-08-23 18:49:35 +0900

    source code

---
+++ demo/demo_stack.py
@@ -0,0 +1,49 @@
+from aws_cdk import (core,
+                     aws_apigateway as apigateway,
+                     aws_lambda as lambda_,
+                     aws_s3 as s3,
+                     aws_s3_deployment as s3deploy)
+
+class DemoStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # create s3 bucket
+        bucket = s3.Bucket(self, "cdkdemobucket",
+                    block_public_access = s3.BlockPublicAccess(
+                        block_public_acls = True,
+                        block_public_policy = True,
+                        ignore_public_acls = True,
+                        restrict_public_buckets = True
+                    ),
+                    bucket_name = "cdkdemobucket",
+                    )
+
+        # upload local file to s3 bucket
+        deploy = s3deploy.BucketDeployment(self, 'DeployLocal',
+                    sources = [s3deploy.Source.asset('resources/s3')],
+                    destination_bucket = bucket)
+
+        handler = lambda_.Function(self, "toppage",
+                    runtime=lambda_.Runtime.PYTHON_3_7,
+                    code=lambda_.Code.asset("resources/webapp/artifact"),
+                    handler="webapp.main",
+                    environment=dict(
+                        BUCKET=bucket.bucket_name)
+                    )
+
+        bucket.grant_read_write(handler)
+
+        #api gateway
+        api = apigateway.RestApi(self, "demo page",
+                    rest_api_name="demo page"
+                    )
+#        api.root.add_resource("{id}")
+        get_top_page = apigateway.LambdaIntegration(handler,
+                    request_templates={"application/json": '{ "statusCode": "200" }'}
+                    )
+
+        api.root.add_method("GET", get_top_page)
+#        api.root.add_method("POST", get_top_page)
+#        api.root.add_method("DELETE", get_top_page)
---
+++ resources/webapp/webapp.py
@@ -0,0 +1,81 @@
+import json
+import boto3
+import logging
+import io
+import base64
+import jinja2
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+s3 = boto3.resource('s3')
+
+def main(event, context):
+    if(not event):
+        return {
+            'isBase64Encoded': False,
+            'headers': {"Content-Type": "*/*"},
+            'statusCode': 500,
+            'body': "event is null"
+        }
+
+    method = event['requestContext']['httpMethod']
+
+    if(method == 'GET'):
+        return get_page()
+
+def get_template():
+    s3obj = s3.Object('cdkdemobucket', 'top/index.html').get()
+    page = s3obj['Body'].read().decode(encoding='utf-8')
+    template = jinja2.Template(page)
+
+    data = {}
+
+    bucket = s3.Bucket('cdkdemobucket')
+    objs = bucket.meta.client.list_objects_v2(Bucket = bucket.name, Prefix = '')
+
+    for obj in objs.get('Contents'):
+        data[obj.get('Key')] = 'http://aaa'
+    
+    template.render(data)
+
+    res =  {
+        'isBase64Encoded': True,
+        'statusCode': 200,
+        'headers': {"Content-Type": "*/*"},
+        'body': template.render(data).decode(encoding='utf-8')
+    }
+    return res
+
+def get_page():
+    s3obj = s3.Object('cdkdemobucket', 'top/index.html').get()
+    page = s3obj['Body'].read()
+
+    res =  {
+        'isBase64Encoded': True,
+        'statusCode': 200,
+        'headers': {"Content-Type": "*/*"},
+        'body': page.decode(encoding='utf-8')
+    }
+    return res
+
+""" 
+    if(event["action"] == "Create"):
+        return {
+            'statusCode':200
+            'body':"Create"
+        }
+
+    if(event["action"] == "Destroy"):
+        return {
+            'statusCode':200
+            'body':"Destroy"
+        } """
+
+
+"""     bucket = "cdkdemobucket/top"
+    key = "testdata.txt"
+    file_contents = "testdata"
+
+    obj = s3.Object(bucket, key)
+    obj.put(Body = file_contents)
+    return """
